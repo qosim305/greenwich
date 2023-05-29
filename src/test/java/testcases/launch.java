08137009d237aa34377a62a213aad7950d845a44
kazeem <qosim305@gmail.com>
2023-05-30 00:13:08 +0100

    comitted

---
+++ src/test/java/testcases/launch.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launch {
+ 
+	public static <Actions> void main(String[] args) {
+	
+		 WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.gre.ac.uk/");
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
